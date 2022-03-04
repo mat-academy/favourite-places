78f0e5a12a47d3edfdd0130adf4ceb9712f16ed4
matbat10 <97300316+matbatten@users.noreply.github.com>
2022-03-04 11:20:54 +0000

    added all the format

--- src/components/PlaceEntry.tsx
+++ src/components/PlaceEntry.tsx
@@ -18,10 +18,12 @@ function PlaceEntry(props: PlaceEntryProps): JSX.Element {
         Location: {props.placeName} - {props.countryName}
       </h3>
       {/*  a main image (URL) */}
-      <img src={props.mainImage} className="images" alt={props.imageDesc}/>
+      <img src={props.mainImage} className="images" alt={props.imageDesc} />
       <br />
       {/* a link to the rough location on google maps (or alternative) (URL) */}
-      <a href={props.mapLink} className="links">See {props.placeName} in Google maps!</a> 
+      <a href={props.mapLink} className="links">
+        See {props.placeName} in Google maps!
+      </a>
       {/* some text explaining why you like the place */}
       <h4>{props.placeBio}</h4>
     </main>
